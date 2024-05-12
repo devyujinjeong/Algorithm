7dbc8fb3c14a307f5f6116af104be799c2c7d12d
devyujinjeong <136146110+devyujinjeong@users.noreply.github.com>
2024-05-12 15:42:47 +0900

    [Silver III] Title: 주유소, Time: 1180 ms, Memory: 188964 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
+		int N = sc.nextInt();
 		
-		int[] length = new int[n-1]; 
-		int[] oil = new int[n];
+		Long[] length = new Long[N-1]; 
+		Long[] oil = new Long[N];
 		
-		for (int i=0; i<n-1; i++) {length[i]=sc.nextInt();} // 도시 사이의 거리
+		for (int i=0; i<N-1; i++) {length[i]=sc.nextLong();} // 도시 사이의 거리
 		
-		for (int i=0; i<n; i++) {oil[i]=sc.nextInt();} // 기름값 (1L당 금액)
+		for (int i=0; i<N; i++) {oil[i]=sc.nextLong();} // 기름값 (1L당 금액)
 		
-		int total = length[0]*oil[0]; // 가장 처음에는 다음 도시까지 갈 기름은 충전해야 함
-		int min = oil[0];
+		Long total = length[0]*oil[0]; // 가장 처음에는 다음 도시까지 갈 기름은 충전해야 함
+		Long min = oil[0];
 		
-		for (int i=1; i<n-1; i++) {
+		for (int i=1; i<N-1; i++) {
 			if(min<oil[i]) { // 전 도시의 기름값이 더 저렴할 경우
 				total+=min*length[i]; 
 			}else {
 				min = oil[i];
 				total+=min*length[i];
 			}
-		}
-		
-		System.out.println(total);
-		
+		}		
+		System.out.println(total);		
 	}
-	
 }
