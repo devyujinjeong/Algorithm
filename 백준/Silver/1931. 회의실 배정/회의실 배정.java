43e34e5aa4170473658dd96ef1d6a1367d230afb
devyujinjeong <136146110+devyujinjeong@users.noreply.github.com>
2024-07-04 14:29:10 +0900

    [Silver I] Title: 회의실 배정, Time: 1224 ms, Memory: 180964 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 회의의 개수와 시간 입력하기
+        int n = sc.nextInt();
+        int[][] meetings = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            meetings[i][0] = sc.nextInt(); // 시작 시간
+            meetings[i][1] = sc.nextInt(); // 종료 시간
+        }
+
+        // 종료 시간 기준으로 정렬, 종료 시간이 같을 경우엔 시작 시간 기준으로 정렬
+        Arrays.sort(meetings, (o1, o2) -> (o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]));
+
+        int count = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (meetings[i][0] >= endTime) { // 회의 시작 시간이 이전 회의 종료 시간보다 크거나 같다면
+                endTime = meetings[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
