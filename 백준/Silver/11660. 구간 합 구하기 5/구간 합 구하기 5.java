30ceee47e870eed142ce0a3d9841dacd0ef9aa14
devyujinjeong <136146110+devyujinjeong@users.noreply.github.com>
2024-07-11 13:59:16 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1380 ms, Memory: 129380 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,27 +1,30 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
     
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         
-        int[][] S = new int[N][N + 1];
-        int sum = 0;
+        int[][] A = new int[N+1][N+1];
         
-        for (int i = 0; i <N; i++) {
+        for (int i = 1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                S[i][0] = 0;
-                S[i][j] = S[i][j - 1] + Integer.parseInt(st.nextToken());
+            for (int j = 1; j<=N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int[][] D = new int[N+1][N+1];
+        
+        for (int i = 1; i<=N; i++) {
+            for (int j = 1; j<=N; j++) {
+                D[i][j] = D[i][j-1]+D[i-1][j]-D[i-1][j-1]+A[i][j];
             }
         }
         
@@ -31,12 +34,9 @@ public class Main {
         	int b = Integer.parseInt(st.nextToken());
         	int c = Integer.parseInt(st.nextToken());
         	int d = Integer.parseInt(st.nextToken());        	
-        	
-        	for(int j=a-1; j<=c-1; j++) {
-        		sum += S[j][d] - S[j][b-1];
-        	}
+        
+        	int sum = D[c][d] - D[a-1][d] - D[c][b-1] + D[a-1][b-1];
         	System.out.println(sum);
-            sum = 0;
         }
     }
 }
