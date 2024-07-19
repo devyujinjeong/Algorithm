034ceb92210391acd7c0315d09bacd58a09bdb52
devyujinjeong <136146110+devyujinjeong@users.noreply.github.com>
2024-07-19 21:20:08 +0900

    [Silver II] Title: 스택 수열, Time: 344 ms, Memory: 27860 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuffer sb = new StringBuffer();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		int[] numArray = new int[N];
 		Stack<Integer> numStack = new Stack<>();
 		int num =1;
 		boolean result = true; 
 		
 		for(int i=0; i<N; i++) {
-			numArray[i] = sc.nextInt(); // 만들고자 하는 수열을 배열에 저장하기
+			numArray[i] = Integer.parseInt(br.readLine());; // 만들고자 하는 수열을 배열에 저장하기
 		}
 				
 		for(int i=0; i<N; i++) {
@@ -28,7 +30,6 @@ public class Main {
 			}else {
 				int n = numStack.pop(); // 일단 가장 위에 있는 숫자를 빼야함
 				if(n>a) {
-					System.out.println("NO");
 					result = false;
 					break;
 				}else {
@@ -39,6 +40,8 @@ public class Main {
 
 		if (result) {
 			System.out.println(sb.toString());
+		}else {
+			System.out.println("NO");
 		}
 	}
 }
