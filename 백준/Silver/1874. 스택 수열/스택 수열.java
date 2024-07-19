264e20822fa35ea873c248ec725b5913fc552e2d
devyujinjeong <136146110+devyujinjeong@users.noreply.github.com>
2024-07-19 21:08:16 +0900

    [Silver II] Title: 스택 수열, Time: 1040 ms, Memory: 106896 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,49 +1,44 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
 		
 		int N = sc.nextInt();
 		int[] numArray = new int[N];
-		ArrayList<String> signList = new ArrayList<String>();
 		Stack<Integer> numStack = new Stack<>();
+		int num =1;
+		boolean result = true; 
 		
 		for(int i=0; i<N; i++) {
 			numArray[i] = sc.nextInt(); // 만들고자 하는 수열을 배열에 저장하기
 		}
 				
-		int num =1;
-		
 		for(int i=0; i<N; i++) {
 			int a = numArray[i];
 			if(num<=a) {
 				while(num<=a) {
 					numStack.push(num++);
-					signList.add("+");
+					sb.append("+\n");
 				}
 				numStack.pop(); // 가장 나중에 들어간 숫자 빼기 
-				signList.add("-");
+				sb.append("-\n");
 			}else {
 				int n = numStack.pop(); // 일단 가장 위에 있는 숫자를 빼야함
 				if(n>a) {
-					signList.add("NO");
+					System.out.println("NO");
+					result = false;
 					break;
 				}else {
-					signList.add("-");
+					sb.append("-\n");
 				}
-			}
-			
+			}			
 		}
 
-		if(signList.contains("NO")) {
-			System.out.println("NO");
-		}else {
-			for(String i:signList) {
-				System.out.println(i);
-			}
-		}		
+		if (result) {
+			System.out.println(sb.toString());
+		}
 	}
 }
