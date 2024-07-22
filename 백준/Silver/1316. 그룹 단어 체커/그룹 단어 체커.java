743a7015916992e3b99d40b1ed57639a21485058
devyujinjeong <136146110+devyujinjeong@users.noreply.github.com>
2024-07-22 15:52:45 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -13,20 +13,15 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			String word = br.readLine();
 			ArrayList<Character> wordChar = new ArrayList<Character>();
-			char first = word.charAt(0);
-			wordChar.add(first);
+			char now = word.charAt(0);
+			wordChar.add(now);
 			
 			for (int j = 1; j < word.length(); j++) {
-				if (word.charAt(j) == first) {
+				if(word.charAt(j)!=now&&wordChar.contains(word.charAt(j))) {
+					break;
+				}else {
 					wordChar.add(word.charAt(j));
-					first = word.charAt(j);
-				} else {
-					if (wordChar.contains(word.charAt(j))) { // 알파벳이 전과 다른데, 알파벳을 wordChar에서 포함하고 있는 경우
-						break;
-					} else {
-						wordChar.add(word.charAt(j));
-						first = word.charAt(j);
-					}
+					now = word.charAt(j);					
 				}
 			}
 			
@@ -34,7 +29,6 @@ public class Main {
 				count++;
 			}
 		}
-		
 		System.out.println(count);
 	}
 }
