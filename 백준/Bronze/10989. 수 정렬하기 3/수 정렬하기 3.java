d83ce3c230e462a4a16d5a04361fc7400b4e9877
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-07-31 21:14:21 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2704 ms, Memory: 346264 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(A);
+		
+		for(int i=0; i<N; i++) {
+			bw.write(A[i]+"\n");
+		}
+
+		bw.flush();
+		bw.close();
+    }
+}
