541dbad37335e726fd7e5dcf7be38ae411e7385b
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-08-08 13:31:18 +0900

    [Silver IV] Title: 수 찾기, Time: 1172 ms, Memory: 59100 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A); // 데이터 정렬하기
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<M; i++) {
+			int target = Integer.parseInt(st.nextToken());
+			boolean result = false;
+			
+			int first = 0;
+			int end = N-1;
+			
+			while(first<=end) {
+				int mid = (first+end)/2;
+				if(A[mid]>target) {
+					end = mid -1;
+				}else if(A[mid]<target) {
+					first = mid+1;
+				}else {
+					result = true;
+					break;
+				}				
+			}
+			
+			System.out.println(result?1:0);
+		}
+	}
+}
