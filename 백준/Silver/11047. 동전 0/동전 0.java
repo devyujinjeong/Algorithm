073b5b3db65eb5bbed28085a535e77e1459bea77
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-08-11 14:36:40 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -3,29 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int[] coin = new int[N]; // 동전을 저장하는 배열
 		int count = 0;
-		
-		for(int i=0 ;i<N; i++) {
+        
+		for (int i = 0; i < N; i++) {
 			coin[i] = Integer.parseInt(br.readLine());
 		}
-		
-		while(K!=0) {
-			for(int i=N-1; i>=0; i--) {
-				if(coin[i]<=K) { 
-					count += K/coin[i];
-					K = K%coin[i];
-				}
+
+		for (int i = N - 1; i >= 0; i--) {
+			if (coin[i] <= K) {
+				count += K / coin[i];
+				K = K % coin[i];
 			}
 		}
-		
 		System.out.println(count);
 	}
 }
