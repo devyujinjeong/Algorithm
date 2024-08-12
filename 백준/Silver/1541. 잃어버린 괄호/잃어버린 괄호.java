2acb285bc4f94399ca0d720604dc195d9cd05dcc
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-08-12 15:17:45 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,54 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String cal = br.readLine();
+		String[] str = cal.split("\\-"); // -를 기준으로 문자 자르기
 		
-        String[] numArr = cal.split("-|\\+"); // +와 -를 기준으로 문자 자르기
-        
-        int result = 0;
-        int n = numArr.length;
-        
-        ArrayList<Integer> num = new ArrayList<Integer>(); // 숫자를 담아두는 arraylist
-        ArrayList<Character> sumOrSub = new ArrayList<Character>(); // 기호를 담아두는 arraylist
-        
-        for (int i=0; i<n; i++) {
-        	num.add(Integer.parseInt(numArr[i]));
-        }
-
-        for(int i=0; i<cal.length(); i++) {
-        	if(cal.charAt(i)=='+'||cal.charAt(i)=='-') {
-        		sumOrSub.add(cal.charAt(i));
-        	}
-        }
-        
-        while(sumOrSub.contains('+')) {
-	        for(int i=0; i<n-1; i++) {
-	            if(sumOrSub.get(i)=='+') {
-	                num.set(i, num.get(i) + num.get(i + 1));
-	                sumOrSub.remove(i); // '+' 문자를 처리한 후 제거
-	                num.remove(i + 1); // 이미 더해진 다음 숫자를 제거.
-	                n--; // 목록의 크기가 줄어들었으므로 n을 감소
-	            }
-	        }
-        }
-        
-        if(num.size()==1) {
-        	result = num.get(0);
-        }else {
-        	result = num.get(0);
-        	for(int i=1; i<num.size(); i++) {
-        		result -= num.get(i);
-        	}
-        }
-
-        System.out.println(result);
+		for(int i=0 ;i<str.length; i++) {
+			if(str[i].contains("+")) {
+				String[] plus = str[i].split("\\+");
+				int num = 0;
+				for(int j=0; j<plus.length; j++) {
+					num+=Integer.parseInt(plus[j]);
+				}
+				str[i] = Integer.toString(num);
+			}
+		}
+		
+		int result = Integer.parseInt(str[0]);
+		for(int i=1; i<str.length; i++) {
+			result -= Integer.parseInt(str[i]);
+		}
+		
+		System.out.println(result);
 	}
-
 }
-
