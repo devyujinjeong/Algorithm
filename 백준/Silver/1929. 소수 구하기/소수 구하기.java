21c0eb19cc7100cb4bb630a044d21224e8eb1144
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-08-14 15:55:46 +0900

    [Silver III] Title: 소수 구하기, Time: 264 ms, Memory: 25224 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int M = Integer.parseInt(st.nextToken());
@@ -13,10 +16,9 @@ public class Main {
 
 		int[] num = new int[N+1];
 		
-		for (int i = 2; i <= N; i++) {
+		for (int i = 2	; i <= N; i++) {
 			num[i] = i;
-		}
-		
+		}		
 		
 		for(int i=2; i<=Math.sqrt(N); i++) {
 			if(num[i]==0) {
@@ -30,8 +32,11 @@ public class Main {
 		
 		for(int i=M; i<=N; i++) {
 			if(num[i]!=0) {
-				System.out.println(num[i]);
+				bw.write(num[i]+"\n");
 			}
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
