c97b70f5f3ba0b2157bfd51e54205ef249ffe962
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-08-14 19:05:40 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 300 ms, Memory: 59100 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] num = new int[10000001];
+		int M = num.length - 1;
+
+		for (int i = 2; i <= M; i++) {
+			num[i] = i;
+		}
+
+		for (int i = 2; i <= Math.sqrt(M); i++) {
+			if (num[i] == 0) {
+				continue;
+			}
+
+			for (int j = i + i; j <= M; j = j + i) {
+				num[j] = 0;
+			}
+		}
+
+		int i = N;
+		while (true) {
+			if (num[i] != 0) {
+				int result = num[i];
+				if (isPalindrome(result)) {
+					System.out.println(result);
+					break;
+				}
+			}
+			i++;
+		}
+	}
+
+	static boolean isPalindrome(int find) {
+		char[] c = String.valueOf(find).toCharArray();
+
+		int start = 0;
+		int end = c.length - 1;
+
+		while (start < end) {
+			if (c[start] != c[end]) {
+				return false;
+			}
+			start++;
+			end--;
+		}
+
+		return true;
+	}
+}
