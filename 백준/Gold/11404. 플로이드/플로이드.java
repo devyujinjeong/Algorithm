f9f69d867be56a68170ecb320e8fe79e3349de95
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-09-01 17:49:54 +0900

    [Gold IV] Title: 플로이드, Time: 508 ms, Memory: 45876 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int inf = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+
+		int[][] D = new int[N + 1][N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (i == j) {
+					D[i][j] = 0;
+				} else {
+					D[i][j] = inf;
+				}
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+
+			// 도착지가 중복되는 경우, 요금이 더 적은 경우만 저장하기
+			if (D[s][e] > p) {
+				D[s][e] = p;
+			}
+		}
+
+		for (int k = 1; k <= N; k++) {
+			for (int s = 1; s <= N; s++) {
+				for (int e = 1; e <= N; e++) {
+					if (D[s][k] != inf && D[k][e] != inf) {
+						D[s][e] = Math.min(D[s][e], D[s][k] + D[k][e]);
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (D[i][j] == inf) {
+					D[i][j] = 0;
+				}
+				System.out.print(D[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
