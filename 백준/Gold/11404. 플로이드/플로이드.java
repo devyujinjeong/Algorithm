6420b834e51e91e08a44c5c2f5b6e77d8bdc7ffd
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-09-01 17:53:31 +0900

    [Gold IV] Title: 플로이드, Time: 416 ms, Memory: 44532 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,6 +10,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
@@ -51,9 +54,12 @@ public class Main {
 				if (D[i][j] == inf) {
 					D[i][j] = 0;
 				}
-				System.out.print(D[i][j] + " ");
+				bw.write(D[i][j]+" ");
 			}
-			System.out.println();
+			bw.write("\n");
 		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
