3ba349173c237b9d96b6a523d014342619048dff
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-10-02 17:16:27 +0900

    [Gold III] Title: LCA, Time: 1320 ms, Memory: 46312 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -46,7 +44,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
@@ -78,9 +76,8 @@ public class Main {
 			int num1 = Integer.parseInt(st.nextToken()); 
 			int num2 = Integer.parseInt(st.nextToken()); 
 			
-			bw.write(LCA(num1, num2)+"\n");
+			sb.append(LCA(num1, num2)+"\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
