0d5335a47b1c4bc3537208f0cacb5db982b0bd3c
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-10-04 21:32:11 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		long[][] D = new long[N+1][2];
 		
 		D[1][0]=0;
@@ -13,7 +14,8 @@ public class Main {
 		for(int i=2; i<=N; i++) {
 			D[i][0] = D[i-1][0] + D[i-1][1];
 			D[i][1] = D[i-1][0];
-		}	
+		}
+		
 		System.out.println(D[N][0] + D[N][1]);
 	}
 }
