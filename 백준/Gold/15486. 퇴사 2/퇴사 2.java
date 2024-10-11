8061d6ed67696a06e62b1fe9e906819df3be9a79
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-10-11 18:14:40 +0900

    [Gold V] Title: 퇴사 2, Time: 656 ms, Memory: 321684 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		long[] D = new long[N + 2];
+		int[] T = new int[N + 1];
+		int[] P = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = N; i > 0; i--) {
+			if (i + T[i] > N + 1) { // 지정된 날까지 일을 끝내지 못하는 경우
+				D[i] = D[i + 1];
+			} else {
+				D[i] = Math.max(D[i + 1], D[i + T[i]] + P[i]);
+			}
+		}
+
+		System.out.println(D[1]);
+
+	}
+}
