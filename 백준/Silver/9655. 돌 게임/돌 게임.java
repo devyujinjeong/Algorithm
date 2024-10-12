c966510e0f28b60e9fa15657cc12b4b652c6ca31
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2024-10-13 02:02:16 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-		int N = sc.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		String[] D = new String[N+1];
 		
 		D[1] = "SK";
