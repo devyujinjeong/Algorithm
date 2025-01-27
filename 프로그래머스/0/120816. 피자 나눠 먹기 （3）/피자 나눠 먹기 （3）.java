1d9e0e9cb7bd4df7b33152383beab80c7d6185d1
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 17:41:25 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,7 +1,9 @@
 class Solution {
     public int solution(int slice, int n) {
+        // n을 slice로 나눴을 때, 
+        // (1) 나머지가 0인 경우 : 인원수를 슬라이스 수로 나누면 됨
+        // (2) 나머지가 0이 아닌 경우 : 인원수를 슬라이스 수로 나누고 + 1
         int answer = n%slice==0?n/slice:n/slice+1;
-        
         
         return answer;
     }
