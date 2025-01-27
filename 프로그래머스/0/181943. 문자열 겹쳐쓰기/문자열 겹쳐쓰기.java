b0a7700ef7195a0f7d407e04c85c07a1e3bc962b
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 16:37:03 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 15.31 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -3,13 +3,9 @@ class Solution {
         String answer = "";
         
         // 1. my_string을 char로 바꾼 뒤 배열에 저장하기
+        char[] word = my_string.toCharArray();
+        
         // 2. s부터 overwrite_string의 길이만큼 글자 바꾸기
-        char[] word = new char[my_string.length()];
-        
-        for(int i=0; i<my_string.length();i++){
-            word[i] = my_string.charAt(i);
-        }
-        
         for(int i=s; i<s+overwrite_string.length(); i++){
             word[i] = overwrite_string.charAt(i-s);
         }
