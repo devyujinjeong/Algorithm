845c8135cc432daf7bd045e70da6d7327b0c852d
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 17:17:11 +0900

    [level 0] Title: 배열의 평균값, Time: 0.05 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,13 +1,14 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-
         int len = numbers.length;
-
+        
+        // 1. 숫자들을 다 더하기
         for(int i=0; i<len; i++){
             answer+=numbers[i];
         }
 
+        // 2. answer에 저장된 숫자를 숫자의 개수로 나누기
         answer = answer/len;
         return answer;
     }
