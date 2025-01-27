d499ca0d860f04c6aaed3fcf83a65af25fd8a846
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 20:38:17 +0900

    [level 1] Title: 소수 만들기, Time: 1.92 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    // 소수 판정 메소드
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public int solution(int[] nums) {
+        int n = nums.length;
+        int answer = 0;
+
+        // 3개의 숫자 조합을 만들어 소수 판정
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
