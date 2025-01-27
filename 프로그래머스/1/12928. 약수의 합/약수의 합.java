61f57a8ec9f41c3babd914b8d72d48c5fe92a9bf
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 20:56:23 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,6 +2,8 @@ class Solution {
     public int solution(int n) {
         int answer = n;
         
+        // 1을 제외하고 남은 공약수 후보들 중 가장 작은 것은 2이므로
+        // n/2까지만 범위를 지정해 조사하면 됨
         for(int i=1; i<=n/2; i++){
             if(n%i==0){
                 answer+=i;
