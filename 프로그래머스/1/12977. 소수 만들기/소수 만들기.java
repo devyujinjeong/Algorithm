763fa8aa52fa8247a323607fefe61b0c2b6abd57
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 20:40:42 +0900

    [level 1] Title: 소수 만들기, Time: 2.42 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,9 +1,10 @@
 import java.util.*;
 
 class Solution {
-    // 소수 판정 메소드
+    // 1. 소수 판정 메소드 구현하기
     static boolean isPrime(int num) {
-        if (num < 2) return false;
+        // 만약 num의 제곱근보다 작거나 같은 자연수(2부터 시작) 그 무엇으로도 나눠지지 않으면
+        // prime임을 이용
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) return false;
         }
@@ -14,7 +15,8 @@ class Solution {
         int n = nums.length;
         int answer = 0;
 
-        // 3개의 숫자 조합을 만들어 소수 판정
+        // 3개의 숫자 조합을 만들어 소수 판정하기 
+        // case 크기가 크지 않아서 그냥 이렇게 써도 될듯
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 for (int k = j + 1; k < n; k++) {
