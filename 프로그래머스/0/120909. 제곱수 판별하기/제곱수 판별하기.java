1d7d7c8f39d5069f5fe50ec3f1c2f9d58639f9c2
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-27 17:51:00 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -2,7 +2,10 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
+        // 1. n의 제곱근 구하고 int로 자료형 변환하기
         int num = (int)Math.sqrt(n);
+        
+        // 2. num을 제곱한 수와 n을 비교하기
         answer = num*num ==n?1:2;
         
         return answer;
