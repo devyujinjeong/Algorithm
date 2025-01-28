2dc2ab2738ca5255aee0c83919f77b9d8190e7a2
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-01-28 16:47:52 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] solution(long n) {
         // 1. n을 string으로 변환하기
-        // 2. 1에서 변환한 값을 배열에 거꾸로 저장하기 + int로 형변환
+        // 2. 1에서 변환한 값을 char형태로 분리해서 배열에 저장하기
+        // 3. char의 경우 int로 자동형변환이 됨. 그리고 '1'은 49를 의미
         String num = Long.toString(n);
         int len = num.length();
         
