3287e1c582ee41c88d7be9ce603264ed33e3ec77
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-07 14:00:51 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -9,19 +9,13 @@ class Solution {
             len++;
         }
                 
-        int[] ternary = new int[len+1];
         
         for(int i=len; i>=0; i--){
             int div = (int)Math.pow(3,i);
-            // 자리별로 0,1,2중 어떤 값이 나오는지 계산하기
             int temp = n/div;
-            
-            // 숫자 뒤집기
-            ternary[len-i] = temp;
+
+            answer += (int)Math.pow(3,len-i)*temp;
             n -= temp*div;
-            
-            // 10진법으로 다시 바꾸기
-            answer += (int)Math.pow(3,len-i)*ternary[len-i];
         }
 
         
