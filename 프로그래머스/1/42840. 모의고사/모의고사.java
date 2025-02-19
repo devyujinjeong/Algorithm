94358e75aa8a1436c9dd0c6b4458d54376ed1334
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-19 11:07:47 +0900

    [level 1] Title: 모의고사, Time: 1.89 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public  ArrayList<Integer> solution(int[] answers) {
+        ArrayList<Integer> answer =  new ArrayList<Integer>();
+
+        String[] repeating = {"12345","21232425","3311224455"};
+        int[] correct = new int[3];
+        int max = 0;
+        
+        for(int j=0; j<3; j++){
+            int len = repeating[j].length();
+            for(int i=0; i<answers.length; i++){
+                if(answers[i]==repeating[j].charAt(i%len)-48){
+                    correct[j]++;
+                }
+            }
+            
+            if(max<correct[j]){
+                max = correct[j];
+            }
+        }
+        
+        for(int i=0; i<3; i++){
+            if(correct[i]==max){
+                answer.add(i+1);
+            }
+        }        
+        
+        return answer;
+    }
+}
