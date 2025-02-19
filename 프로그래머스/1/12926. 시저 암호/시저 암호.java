53888f044894fecd064e9901bc73d3848b66095d
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-19 16:31:25 +0900

    [level 1] Title: 시저 암호, Time: 31.51 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,32 +2,18 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         
-        char[] word = s.toCharArray();
-        
-        // 아스키 코드를 이용해 문제 해결
-        for(int i=0; i<word.length; i++){
-            // 공백이 아닌 경우만 검사하기 위해서
-            if(word[i]!=' '){
-                // word[i]를 int로 바꾸고 여기에 n을 저장
-                int temp = word[i]+n;
-                
-                if(word[i]<=90){ // 대문자인 경우
-                    if(temp>90&&temp<97){
-                        temp -= 26;
-                    }else if(temp>=97){
-                        temp = word[i]-26+n;
-                    }
-                }else if(word[i]>=97){ // 소문자인 경우
-                    if(temp>122){
-                        temp -= 26;
-                    }
-                }
-                word[i] = (char)temp;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
             
-            answer += word[i];
+            if (ch >= 'A' && ch <= 'Z') { 
+                answer += (char) ((ch - 'A' + n) % 26 + 'A');
+            } else if (ch >= 'a' && ch <= 'z') { 
+                answer += (char) ((ch - 'a' + n) % 26 + 'a');
+            } else {
+                answer += ch;
+            }
         }
-
+        
         return answer;
     }
 }
