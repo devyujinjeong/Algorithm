3c8421aaf4a31cc8d192f1a4f395fc257e5037bf
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-20 11:21:12 +0900

    [level 1] Title: 체육복, Time: 1.22 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {        
+        int answer = n - lost.length;
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        for (int i = 0; i < reserve.length; i++) {
+            for (int j = 0; j < lost.length; j++) {
+                if (reserve[i] == lost[j]) {
+                    answer++; 
+                    reserve[i] = 0; 
+                    lost[j] = 0;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < reserve.length; i++) {
+            if (reserve[i] == 0) continue; 
+            for (int j = 0; j < lost.length; j++) {
+                if (lost[j] == 0) continue; 
+                if (reserve[i] == lost[j] - 1 || reserve[i] == lost[j] + 1) {
+                    answer++;
+                    lost[j] = -1; 
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
