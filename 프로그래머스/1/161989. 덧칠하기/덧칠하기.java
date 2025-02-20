c9cdb478af854fd9e711af466c00b9f9d7048d58
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-20 14:00:56 +0900

    [level 1] Title: 덧칠하기, Time: 3.36 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,16 +2,11 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         int finished = 0;
-        int len = section.length;
         
-        for(int i=0; i<len; i++){
+        for(int i=0; i<section.length; i++){
             if(finished<section[i]){
                 finished = section[i]+m-1;
                 answer++;
-            }
-
-            if(finished>=section[len-1]){
-                break;
             }
         }
         
