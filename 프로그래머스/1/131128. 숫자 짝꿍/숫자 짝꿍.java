8a9a2926abba895fc51ccacaa4ec02621dcf5072
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-27 12:17:15 +0900

    [level 1] Title: 숫자 짝꿍, Time: 100.36 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -2,26 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(String X, String Y) {
-        StringBuilder answer = new StringBuilder();
         int[] xCount = new int[10];
         int[] yCount = new int[10];
-        
-        for (char c : X.toCharArray()) { xCount[c - '0']++; }
-        for (char c : Y.toCharArray()) { yCount[c - '0']++; }
 
+        for (char c : X.toCharArray()) xCount[c - '0']++;
+        for (char c : Y.toCharArray()) yCount[c - '0']++;
+
+        StringBuilder answer = new StringBuilder();
+        
         for (int i = 9; i >= 0; i--) {
-            int min = Math.min(xCount[i], yCount[i]);
-            answer.append(String.valueOf(i).repeat(min));
+            int common = Math.min(xCount[i], yCount[i]);
+            for(int j=0; j<common; j++){
+                answer.append(Integer.toString(i));
+            }
         }
 
         if (answer.length() == 0) {
             return "-1";
         }
         
-        if (answer.toString().matches("^0+$")) {
+        if (answer.toString().equals("0".repeat(answer.length()))) {
             return "0";
         }
-        
+
         return answer.toString();
     }
 }
