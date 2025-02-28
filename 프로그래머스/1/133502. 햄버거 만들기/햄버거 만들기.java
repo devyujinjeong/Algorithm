67f359056cfdff2445862d299dca3ce5aaea19fd
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-02-28 14:03:44 +0900

    [level 1] Title: 햄버거 만들기, Time: 50.77 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -8,23 +8,25 @@ class Solution {
 
         for (int data : ingredient) {
             stack.push(data);
-
-            if (stack.size() >= 4) {
+            
+            // 스택의 크기가 4이상인 경우에 조사하기
+            if(stack.size()>=4){
                 boolean match = true;
-                for (int i = 0; i < 4; i++) {
+                for(int i=0; i<4; i++){
                     if (stack.get(stack.size() - 4 + i) != pattern[i]) {
                         match = false;
                         break;
                     }
                 }
-
+                
                 if (match) {
-                    for (int i = 0; i < 4; i++) {
-                        stack.pop();
-                    }
-                    answer++;
+                for (int i = 0; i < 4; i++) {
+                    stack.pop();
+                }
+                answer++;
                 }
             }
+
         }
         return answer;
     }
