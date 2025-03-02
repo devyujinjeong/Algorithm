427324d46d713a0274561665bee82254fa4440ff
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-03-02 23:48:41 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.21 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,35 +1,22 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = health; // 체력
-        int count = 0; // 시전 시간
-        int j = 0; // 공격 인덱스
-        
-        // 마지막 공격이 있는 시간까지 반복
-        for(int i=1; i<=attacks[attacks.length-1][0]; i++){
-            if(i==attacks[j][0]){ // 공격을 받는 시간
-                answer -= attacks[j][1];
-                j++;
+        int answer = health;
+        int count = 0;
+        int j = 0;
+
+        for (int i = 1; i <= attacks[attacks.length - 1][0]; i++) {
+            if (i == attacks[j][0]) { // 공격받는 시간
+                answer -= attacks[j++][1];
                 count = 0;
-            }else{ // 공격을 받지 않는 시간
-                count++;
-                if(answer+bandage[1]<=health){ // 최대 체력을 넘지 않음
-                    answer += bandage[1]; 
-                    
-                    if(count==bandage[0]){
-                        answer += bandage[2]; 
-                        count = 0;
-                    }
-                }else{
-                    answer = health;
-                    if(count==bandage[0]) count = 0;
-                }      
+            } else { // 회복 시간
+                answer = Math.min(health, answer + bandage[1]); // 최대 체력 초과 방지
+                if (++count == bandage[0]) { // 연속 시전 성공
+                    answer = Math.min(health, answer + bandage[2]);
+                    count = 0;
+                }
             }
-            
-            if(answer<=0){
-                return -1;
-            }
+            if (answer <= 0) return -1;
         }
-        
         return answer;
     }
 }
