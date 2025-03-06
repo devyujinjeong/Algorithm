0c3acc1a43163cdac2ef33dcef6967deea6aeb77
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-03-06 15:09:12 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 148 ms, Memory: 15068 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static class Balloon {
@@ -13,13 +16,14 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 풍선 개수
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 풍선 개수
         Deque<Balloon> balloons = new ArrayDeque<>();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int note = sc.nextInt();
+            int note = Integer.parseInt(st.nextToken());
             balloons.add(new Balloon(i + 1, note));
         }
 
@@ -46,5 +50,6 @@ public class Main {
         }
 
         System.out.println(answer);
+        br.close();
     }
 }
