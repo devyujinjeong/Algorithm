852baf20791a4569ac666c02f9aa01a97fece480
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-03-11 17:12:21 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.22 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -6,21 +6,10 @@ class Solution {
             answer[1]+= s.length();
             s = s.replace("0","");
             answer[1]-= s.length();
-            s = getBinaryNum(s.length());
+            s = Integer.toBinaryString(s.length());
             answer[0]++;
         }
  
         return answer;
-    }
-    
-    String getBinaryNum(int x) {
-        StringBuilder sb = new StringBuilder();
-        
-        while (x > 0) {
-            sb.append(x % 2);
-            x /= 2;
-        }
-        
-        return sb.reverse().toString();
     }
 }
