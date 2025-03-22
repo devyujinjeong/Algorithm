d6a5d72e1e7760c21bc3ecc16ed91791c76b3704
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-03-22 21:30:30 +0900

    [level 2] Title: 방문 길이, Time: 18.90 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dirs) {
+        Set<String> path = new HashSet<>();
+        int nowX = 5, nowY = 5;
+
+        for (char temp : dirs.toCharArray()) {
+            int nextX = nowX;
+            int nextY = nowY;
+
+            if (temp == 'U') {
+                nextY++;
+            } else if (temp == 'D') {
+                nextY--;
+            } else if (temp == 'L') {
+                nextX--;
+            } else if (temp == 'R') {
+                nextX++;
+            }
+
+            if (nextX < 0 || nextX > 10 || nextY < 0 || nextY > 10) {
+                continue;
+            }
+
+            String path1 = nowX + "," + nowY + "->" + nextX + "," + nextY;
+            String path2 = nextX + "," + nextY + "->" + nowX + "," + nowY;
+
+            if (!path.contains(path1)) {
+                path.add(path1);
+                path.add(path2);
+            }
+
+            nowX = nextX;
+            nowY = nextY;
+        }
+
+        return path.size() / 2;
+    }
+}
