5b4430fd5a88800fd5a4977a122ce0c2b0671a45
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-03-27 20:18:01 +0900

    [level 2] Title: 할인 행사, Time: 88.08 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,32 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        
-        Map<String, Integer> products = new HashMap<>();
-        
-        for(int i=0; i<want.length; i++){
-            products.put(want[i], number[i]);
-        }
-        
-        for(int i=0; i<=discount.length-10; i++){
-            Map<String, Integer> tempMap = new HashMap<>(products);
-            int count = 0;
-            for(int j=i; j<i+10; j++){
-                String item = discount[j];
-                if(tempMap.containsKey(item)&&tempMap.get(item)>0){
-                    tempMap.put(item, tempMap.get(item)-1);
-                    count++;
-                }
-                
-            }
-
-            if(count==10) {
-                answer++; 
-            }
-        }
-        
-        
         return answer;
     }
 }
