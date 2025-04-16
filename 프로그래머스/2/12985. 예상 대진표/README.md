a64a7347c3af390551bcab9fc17bf08aac4c9719
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-04-16 23:27:02 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 0.03 ms
+메모리: 98 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 21:38:47
+2025년 04월 16일 23:26:58
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,12 +1,15 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution{
+    public int solution(int n, int a, int b){
         int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        // 밑의 계산을 했을 때, 숫자가 같아지는 순간 전 단계가 둘이 대결한 라운드
+        while(a!=b){
+            a = (a+1)/2;
+            b = (b+1)/2;
+            
+            answer++;
+        }
+        
         return answer;
     }
 }
