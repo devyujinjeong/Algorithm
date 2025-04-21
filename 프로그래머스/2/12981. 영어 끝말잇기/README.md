a335b56bd539c6a55f3981570e03bf40732c5e57
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-04-22 00:00:24 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
 
 ### 성능 요약
 
-메모리: 84.2 MB, 시간: 0.10 ms
+메모리: 91.2 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 14:05:45
+2025년 04월 22일 00:00:22
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -6,13 +6,16 @@ class Solution {
 
         Set<String> allWords = new HashSet<>();
     
+        // 첫 번째 단어 넣기
         allWords.add(words[0]);
         for(int i=1; i<words.length; i++){
+            // 만약 앞에 나온 단어라면
             if(allWords.contains(words[i])){
-                answer[0] = i%n+1;
-                answer[1] = i/n+1; 
+                answer[0] = i%n+1; // 몇 번째 사람이
+                answer[1] = i/n+1; // 몇 번째 차례에서 
                 break;
             }else{
+                // 끝나는 글자와 시작하는 글자가 다르다면
                 if(words[i].charAt(0)!=words[i-1].charAt(words[i-1].length()-1)){
                     answer[0] = i%n+1;
                     answer[1] = i/n+1; 
@@ -23,7 +26,8 @@ class Solution {
             }            
         }
         
-
+        // 만약 이 for문을 다 통과하지 않는다면 끝말잇기가 성공한 것으므로
+        // 초기값인 [0,0]이 반환됨
         return answer;
     }
 }
