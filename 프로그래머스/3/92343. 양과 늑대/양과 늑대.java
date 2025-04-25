c3e66360f57077f9ddc502c936d6d08bffd0d434
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-04-25 22:31:38 +0900

    [level 3] Title: 양과 늑대, Time: 7.61 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,57 +1,53 @@
 import java.util.*;
 
 class Solution {
-    private static class Info {
-        int node, sheep, wolf; // 현재 위치, 양의 수, 늑대의 수
-        HashSet<Integer> visited; // 방문 기록
-        
-        public Info(int node, int sheep, int wolf, HashSet<Integer> visited) {
-            this.node = node;
-            this.sheep = sheep;
-            this.wolf = wolf;
-            this.visited = visited;
-        }
-    }
-    
     private static ArrayList<Integer>[] tree;
-    static int answer;
-    
+    private static int answer;
+
     public int solution(int[] info, int[][] edges) {
-        answer = 0;
-        tree = new ArrayList[info.length];
-        for(int i=0; i<tree.length; i++) {
+        int n = info.length;
+        tree = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
             tree[i] = new ArrayList<>();
         }
-        
-        // 연결 관계 저장하기
-        for(int[] edge : edges) {
+
+        for (int[] edge : edges) {
             tree[edge[0]].add(edge[1]);
         }
-        
-        ArrayDeque<Info> queue = new ArrayDeque<>();
-        // 루트 노드는 항상 양이기 때문에 1추가
-        queue.add(new Info(0, 1, 0, new HashSet<>()));
-    
-        while(!queue.isEmpty()) {
-            Info now = queue.poll();
-            answer = Math.max(answer, now.sheep);
-            now.visited.addAll(tree[now.node]);
-            
-            for(int next : now.visited) {
-                HashSet<Integer> set = new HashSet<>(now.visited);
-                set.remove(next);
-                
-                if(info[next] == 1) {
-                    if(now.sheep != now.wolf + 1) {
-                        queue.add(new Info(next, now.sheep, now.wolf+1, set));
-                    }
-                } else {
-                    queue.add(new Info(next, now.sheep+1, now.wolf, set));
-                }
-            }
-        }
-        
+
+        answer = 0;
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+
+        List<Integer> nextNodes = new ArrayList<>();
+        nextNodes.addAll(tree[0]);
+
+        dfs(info, 1, 0, visited, nextNodes);
+
         return answer;
     }
 
+    private void dfs(int[] info, int sheep, int wolf, boolean[] visited, List<Integer> nextNodes) {
+        if (sheep <= wolf) return;
+        answer = Math.max(answer, sheep);
+
+        for (int i = 0; i < nextNodes.size(); i++) {
+            int next = nextNodes.get(i);
+
+            boolean[] visitedCopy = visited.clone();
+            visitedCopy[next] = true;
+
+            List<Integer> nextList = new ArrayList<>(nextNodes);
+            nextList.remove(i);
+            for (int child : tree[next]) {
+                if (!visitedCopy[child]) nextList.add(child);
+            }
+
+            if (info[next] == 0) { // 양
+                dfs(info, sheep + 1, wolf, visitedCopy, nextList);
+            } else { // 늑대
+                dfs(info, sheep, wolf + 1, visitedCopy, nextList);
+            }
+        }
+    }
 }
