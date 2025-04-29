37c71c93e7b592375818513f63d9cd9216b775b2
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-04-29 14:53:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.48 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 56 MB, 시간: 9.68 ms
+메모리: 54.9 MB, 시간: 10.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 28일 23:58:17
+2025년 04월 29일 14:53:35
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,58 +1,58 @@
 import java.util.*;
 
 class Solution {
-
-    // 동서남북 경로를 위한 배열
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 0, 1, -1}; 
+    static int[] dy = {1, -1, 0, 0};
+    static int[][] total;
+    static int[][] maps;
 
     public int solution(int[][] maps) {
-        int answer = 0;
+        int N = maps.length;      
+        int M = maps[0].length;  
+        this.maps = maps;
+
+        total = new int[N][M];
+
+        bfs(new Node(0, 0));
+
+        return total[N - 1][M - 1] == 0 ? -1 : total[N - 1][M - 1];
+    }
+
+    static void bfs(Node node) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        total[node.x][node.y] = 1; 
 
         int N = maps.length;
         int M = maps[0].length;
 
-        // 최단 거리를 저장할 배열 
-        int[][] dist = new int[N][M];
-
-        ArrayDeque<Node> queue = new ArrayDeque<>();
-
-        // 시작점을 큐에 넣고 거리는 1로 초기화
-        queue.addLast(new Node(0, 0));
-        dist[0][0] = 1;
-
         while (!queue.isEmpty()) {
-            Node now = queue.pollFirst();
+            Node temp = queue.poll(); 
+
+            int nowX = temp.x;
+            int nowY = temp.y;
 
             for (int i = 0; i < 4; i++) {
-                int nr = now.r + dx[i];
-                int nc = now.c + dy[i];
+                int nx = nowX + dx[i];
+                int ny = nowY + dy[i]; 
 
-                // 맵을 벗어나는 경우
-                if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
-
-                // 벽인 곳은 건너뛰기
-                if (maps[nr][nc] == 0) continue;
-
-                // 아직 방문하지 않은 곳만 큐에 넣고 거리 업데이트
-                if (dist[nr][nc] == 0) {
-                    queue.addLast(new Node(nr, nc));
-                    dist[nr][nc] = dist[now.r][now.c] + 1;
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if (maps[nx][ny] == 1 && total[nx][ny] == 0) {
+                        total[nx][ny] = total[nowX][nowY] + 1; 
+                        queue.add(new Node(nx, ny)); 
+                    }
                 }
             }
         }
-
-        // BFS를 통해 최단 거리 계산하기
-        return dist[N-1][M-1] == 0 ? -1 : dist[N-1][M-1];
     }
 }
 
 class Node {
-    int r;
-    int c;
+    int x;
+    int y;
 
-    public Node(int r, int c) {
-        this.r = r;
-        this.c = c;
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
