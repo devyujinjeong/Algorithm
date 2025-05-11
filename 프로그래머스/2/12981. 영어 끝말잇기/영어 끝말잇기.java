0d53b8df1daadf45da8519785df251d85695033e
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-05-11 21:43:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -4,10 +4,12 @@ class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
 
+        // 이전에 등장했던 단어인지를 확인하기 위해서 Set 이라는 자료형을 사용
         Set<String> allWords = new HashSet<>();
     
         // 첫 번째 단어 넣기
         allWords.add(words[0]);
+        
         for(int i=1; i<words.length; i++){
             // 만약 앞에 나온 단어라면
             if(allWords.contains(words[i])){
@@ -27,7 +29,7 @@ class Solution {
         }
         
         // 만약 이 for문을 다 통과하지 않는다면 끝말잇기가 성공한 것으므로
-        // 초기값인 [0,0]이 반환됨
+        // 초기값인 [0,0]을 반환
         return answer;
     }
 }
