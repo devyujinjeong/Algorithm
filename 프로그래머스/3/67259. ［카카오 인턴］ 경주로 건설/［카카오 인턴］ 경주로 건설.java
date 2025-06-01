215adcfc012d936179fcee937d5f0584ce02bd2a
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-01 16:17:52 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.65 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -5,10 +5,13 @@ class Solution {
     static final int[] dy = {1, -1, 0, 0};
     static int N;
     static int[][][] cost;
+    static int[][] board;
 
     public int solution(int[][] board) {
+        this.board = board;
         N = board.length;
-        cost = new int[N][N][4]; // 각 위치별, 방향별 최소 비용
+        // 위치별, 방향별 최소 비용 (방향이 4개 이므로)
+        cost = new int[N][N][4];
 
         for (int[][] layer : cost) {
             for (int[] row : layer) {
@@ -16,7 +19,7 @@ class Solution {
             }
         }
 
-        bfs(board);
+        bfs();
 
         int answer = Integer.MAX_VALUE;
         for (int i = 0; i < 4; i++) {
@@ -26,35 +29,38 @@ class Solution {
         return answer;
     }
 
-    static void bfs(int[][] board) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 0, -1, 0));
+    static void bfs() {
+        PriorityQueue<Node> pq = 
+            new PriorityQueue<>(Comparator.comparingInt(n -> n.cost));
+        
+        // 첫 시작점은 방향이 없으니깐 -1로 설정
+        pq.offer(new Node(0, 0, -1, 0));
 
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
 
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
-                if (!isValid(nx, ny, board)) continue;
+                if (!isValid(nx, ny)) continue;
 
                 int newCost = cur.cost;
                 if (cur.direction == -1 || cur.direction == i) {
                     newCost += 100; // 직선
                 } else {
-                    newCost += 600; // 커브
+                    newCost += 600; // 커브 (방향이 달라짐)
                 }
 
                 if (cost[nx][ny][i] > newCost) {
                     cost[nx][ny][i] = newCost;
-                    q.offer(new Node(nx, ny, i, newCost));
+                    pq.offer(new Node(nx, ny, i, newCost));
                 }
             }
         }
     }
 
-    static boolean isValid(int x, int y, int[][] board) {
+    static boolean isValid(int x, int y) {
         return x >= 0 && y >= 0 && x < N && y < N && board[x][y] == 0;
     }
 }
