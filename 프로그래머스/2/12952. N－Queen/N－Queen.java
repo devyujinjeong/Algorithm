8438f7cb0130bdbe20b937da31845c10daa35af9
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-12 15:44:41 +0900

    [level 2] Title: N-Queen, Time: 160.41 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,7 +1,7 @@
 class Solution {
-    private int count = 0;
-    private int n;
-    private int[] queens; // 각 행에서 퀸이 놓인 열의 위치를 기록
+    static int count = 0;
+    static int n;
+    static int[] queens;
 
     public int solution(int n) {
         this.n = n;
@@ -10,31 +10,26 @@ class Solution {
         return count;
     }
 
-    // 백트래킹을 구현한 함수
-    private void backtrack(int x) {
-        // x가 n이라면 퀸을 모두 배치 시킨 것
+    static void backtrack(int x) {
         if (x == n) {
             count++;
             return;
         }
 
-        for (int y = 0; y < n; y++) {
-            if (isPossible(x, y)) {
-                // x행에서의 퀸의 위치는 y
-                queens[x] = y;
+        for (int i = 0; i < n; i++) {
+            queens[x] = i;
+
+            if (isPossible(x)) {
                 backtrack(x + 1);
             }
         }
     }
 
-    // x행 y열에 퀸을 놓을 수 있는지 확인하기
-    // 같은 열과 대각선에 퀸이 있는지 확인
-    private boolean isPossible(int x, int y) {
+    static boolean isPossible(int x) {
         for (int i = 0; i < x; i++) {
-            // 같은 열에 퀸이 있는지 확인
-            if (queens[i] == y) return false; 
-            // 대각선에 퀸이 있는지 확인
-            if (Math.abs(x - i) == Math.abs(y - queens[i])) return false;
+            if (queens[i] == queens[x] || Math.abs(x - i) == Math.abs(queens[x] - queens[i])) {
+                return false;
+            }
         }
         return true;
     }
