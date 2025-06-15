d62995ab6ce93e85f030b91aedb13e85643788ac
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-15 22:34:47 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.53 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 1.99 ms
+메모리: 85.7 MB, 시간: 0.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 30일 19:44:26
+2025년 06월 15일 22:34:47
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,19 +2,18 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        
-        String num = Long.toString(n);
-        char[] d = num.toCharArray();
-        
-        Arrays.sort(d);
-        String e = "";
-        for(int i=num.length()-1; i>=0; i--){
-            e += Integer.toString((int)d[i]-48);
+        // 1. 숫자를 문자열로 바꾸고
+        String[] digits = String.valueOf(n).split("");
+
+        // 2. 내림차순 정렬
+        Arrays.sort(digits, Collections.reverseOrder());
+
+        // 3. 문자열을 합치고 long으로 변환
+        StringBuilder sb = new StringBuilder();
+        for (String digit : digits) {
+            sb.append(digit);
         }
-        
-        answer = Long.parseLong(e);        
-        
-        return answer;
+
+        return Long.parseLong(sb.toString());
     }
 }
