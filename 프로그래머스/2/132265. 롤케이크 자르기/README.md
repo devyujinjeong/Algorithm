6cbf946bc0ebecc0819422630b9a76a37bf3ab8b
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-21 22:54:01 +0900

    [level 2] Title: 롤케이크 자르기, Time: 226.92 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java) 
 
 ### 성능 요약
 
-메모리: 187 MB, 시간: 198.96 ms
+메모리: 182 MB, 시간: 226.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 03일 11:52:39
+2025년 06월 21일 22:54:01
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,35 +4,28 @@ class Solution {
     public int solution(int[] topping) {
         int answer = 0;
 
-        // 오른쪽 토핑: 종류별 개수 카운트
-        HashMap<Integer, Integer> right = new HashMap<>();
-        HashSet<Integer> left = new HashSet<>();
-        
-        for(int i : topping) {
-            right.put(i, right.getOrDefault(i, 0) + 1);
+        Map<Integer, Integer> leftMap = new HashMap<>();
+        Map<Integer, Integer> rightMap = new HashMap<>();
+
+        for (int t : topping) {
+            rightMap.put(t, rightMap.getOrDefault(t, 0) + 1);
         }
-        
-        for(int i = 0; i < topping.length; i++) {
-            // 왼쪽에 토핑 추가
-            left.add(topping[i]);
-            // 오른쪽에서 토핑 하나 제거
-            right.put(topping[i], right.get(topping[i]) - 1);
-            
-            int count1 = left.size();    // 왼쪽 토핑 종류 수
-            
-            // 오른쪽에서 특정 토핑이 다 빠졌다면 종류 수 감소
-            if(right.get(topping[i]) == 0) {
-                right.remove(topping[i]);
+
+        for (int t : topping) {
+            // 왼쪽으로 옮기기
+            leftMap.put(t, leftMap.getOrDefault(t, 0) + 1);
+
+            // 오른쪽에서 제거
+            rightMap.put(t, rightMap.get(t) - 1);
+            if (rightMap.get(t) == 0) {
+                rightMap.remove(t);
             }
-            
-            int count2 = right.size();   // 오른쪽 토핑 종류 수
-            
-            // 양쪽 종류 수가 같으면 정답++
-            if(count1 == count2) {
+
+            if (leftMap.size() == rightMap.size()) {
                 answer++;
             }
         }
-        
+
         return answer;
     }
 }
