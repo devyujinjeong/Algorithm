278f577afa73abf25c7de9ad0bc19633fd2c4029
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-23 21:17:09 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 87.6 MB, 시간: 0.09 ms
+메모리: 71.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 16일 20:09:01
+2025년 06월 23일 21:17:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int i=1; i<=(int)Math.sqrt(yellow); i++){ 
-            if(yellow%i==0 && (i+yellow/i)*2+4==brown){
-                answer[0] = Math.max(i,yellow/i);
-                answer[1] = Math.min(i,yellow/i);
-                break;
-            }
+        int N = (int)Math.sqrt(yellow);
+        
+        for(int i=1; i<=N; i++){
+            int j = yellow/i;
+            if(yellow%i==0 && (i+j)*2 + 4 == brown) {
+                answer[0] = Math.max(i,j)+2;
+                answer[1] = Math.min(i,j)+2;
+            }   
         }
         
-        answer[0]+=2; answer[1]+=2;
         return answer;
     }
 }
