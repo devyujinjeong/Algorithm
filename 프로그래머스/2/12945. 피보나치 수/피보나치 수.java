1059ebf86248ad1321b86a8b68a83d9c140aa637
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-24 16:33:01 +0900

    [level 2] Title: 피보나치 수, Time: 6.59 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,11 @@
 class Solution {
-    public int solution(int n) {        
-        int[] dp = new int[100001];
-        int mod = 1234567;
+    static final int mod = 1234567;
+    public int solution(int n) {
+        int[] dp = new int[1000001];
+        dp[0] = 0; dp[1] = 1;
         
-        dp[0] = 0;
-        dp[1] = 1;
-        
-        for(int i= 2; i<=n; i++){
-            dp[i] = (dp[i-1]%mod + dp[i-2]%mod)%mod;
+        for(int i=2; i<=n; i++) {
+            dp[i] = (dp[i-1] % mod + dp[i-2] % mod) % mod;
         }
         
         return dp[n];
