f2971295d07ce85e0e7952973e6c7cb2c2ef4de4
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-06-25 20:33:24 +0900

    [level 2] Title: 2 x n 타일링, Time: 4.60 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,14 +1,15 @@
 class Solution {
-    public long solution(int n) {
+    public int solution(int n) {        
         long[] dp = new long[n+1];
-        final long mod = 1000000007;
-        dp[1] = 1; 
+        long mod = 1000000007;
+        
+        dp[1] = 1;
         dp[2] = 2;
-
-        for(int i=3; i<=n; i++){
-            dp[i] = (dp[i-1]%mod+dp[i-2]%mod)%mod; 
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] % mod + dp[i-2] % mod) % mod;
         }
-
-        return dp[n];
+        
+        return (int)dp[n];
     }
 }
--- 프로그래머스/2/12900. 2 x n 타일링/README.md
+++ 프로그래머스/2/12900. 2 x n 타일링/README.md
@@ -1,10 +1,10 @@
 # [level 2] 2 x n 타일링 - 12900 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12900) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12900?language=java) 
 
 ### 성능 요약
 
-메모리: 53.9 MB, 시간: 4.37 ms
+메모리: 54.1 MB, 시간: 4.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 19일 17:44:33
+2025년 06월 25일 20:33:22
 
 ### 문제 설명
 
