f7b7a6022c5e9f361dad2a3156a31c636c4c7c04
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-07-02 22:02:35 +0900

    [level 2] Title: 귤 고르기, Time: 83.29 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=java) 
 
 ### 성능 요약
 
-메모리: 91.9 MB, 시간: 78.60 ms
+메모리: 97.1 MB, 시간: 83.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 18일 15:01:26
+2025년 07월 02일 22:02:30
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 1;
+        int answer = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
         
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<tangerine.length; i++){
-            map.put(tangerine[i],map.getOrDefault(tangerine[i],0)+1);
+        // 1. hashmap에 담기
+        for(int i=0; i<tangerine.length; i++) {
+            hm.put(tangerine[i], hm.getOrDefault(tangerine[i], 0) + 1);
         }
         
-        List<Integer> valueList = new LinkedList<>(map.values());
-        valueList.sort((o1,o2) -> o2-o1);
-
-
-        for (Integer value : valueList) {
-            k-=value;
-            if(k>0){
-                answer++;
-            }else{
+        // 2. 개수가 큰 순서대로 정렬하기
+        List<Integer> newList = new ArrayList<>(hm.keySet());
+        newList.sort((o1, o2) -> hm.get(o2).compareTo(hm.get(o1)));
+        
+        // 3. 앞에서 부터 귤의 개수 차감하기
+        for(Integer key : newList) {
+            k-=hm.get(key);
+            answer++;
+            if(k<=0) {
                 break;
             }
         }
