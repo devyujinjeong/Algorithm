d7ec117403a4ec11e54f0685a564684233c9fa37
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-08-05 18:42:52 +0900

    [Silver I] Title: 포도주 시식, Time: 120 ms, Memory: 14944 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -7,18 +7,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] num = new int[N+2];
-        int[] dp = new int[N+2];
+        int[] num = new int[N+1];
+        int[] dp = new int[N+1];
 
         for(int i=1; i<=N; i++) {
             num[i] = Integer.parseInt(br.readLine());
         }
 
         dp[1] = num[1];
-        dp[2] = num[1] + num[2];
 
-        for(int i=3; i<=N; i++) {
-            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + num[i], dp[i-3] + num[i-1] + num[i]));
+        for(int i=2; i<=N; i++) {
+            if(i==2) {
+                dp[i] = num[1] + num[2];
+            } else {
+                // 1. dp[i-1]
+                // 2. 2칸 전의 값(dp[i-2])과 현재 값(num[i]) 더하기
+                // 3. 3칸 전의 답(dp[i-3] + 현재와 이전의 값 (num[i-1] + num[i])
+                dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + num[i], dp[i-3] + num[i-1] + num[i]));
+            }
         }
 
         System.out.println(dp[N]);
