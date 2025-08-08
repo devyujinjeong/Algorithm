c77afef5b6e4cec8eb15cf20cb9d2c5a961e70b6
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-08-08 22:02:11 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 504 ms, Memory: 45856 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,38 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
         for(int i=0; i<N; i++) {
+            StringBuilder sb = new StringBuilder();
             String s = br.readLine();
             String[] words = s.split(" ");
 
-            Queue<String> q = new LinkedList<>();
-
-            for(int j=0; j<words.length; j++) {
-                q.add(words[j]);
+            for(String word : words) {
+                StringBuilder tempW = new StringBuilder(word);
+                sb.append(tempW.reverse()).append(" ");
             }
 
-            while(!q.isEmpty()) {
-                String word = q.poll();
-                char[] alphabet = word.toCharArray();
-                String newWord = "";
-                for(int k = alphabet.length-1; k>=0; k--) {
-                    newWord += alphabet[k];
-                }
-                sb.append(newWord).append(" ");
-            }
-            sb.append("\n");
+            System.out.print(sb);
         }
-
-        System.out.println(sb);
     }
 }
