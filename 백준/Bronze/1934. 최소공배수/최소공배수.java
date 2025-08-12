9588fe88508ef50eae0082b5927ff374a3683b01
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-08-12 20:30:18 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -4,25 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int multi = a*b;
-			int mod = b%a;
-			
-			while(mod!=0) {
-				b = a;
-				a = mod;
-				mod = b%a;
-			}
-			
-			System.out.println(multi/a);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a*b/getGcd(a,b)).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static int getGcd(int a, int b) {
+        int min = Math.min(a,b);
+        int max = Math.max(a,b);
+        int mod = max % min;
+
+        while(mod>0) {
+            max = min;
+            min = mod;
+            mod = max % mod;
+        }
+
+        return min;
+    }
+
 }
