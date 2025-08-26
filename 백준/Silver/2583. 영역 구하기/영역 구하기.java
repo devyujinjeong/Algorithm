c6f1537fc66996f44f1230691932ba4698e77f84
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-08-26 17:26:05 +0900

    [Silver I] Title: 영역 구하기, Time: 116 ms, Memory: 15276 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -3,93 +3,82 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] path;
+
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-    static int M, N, K;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+    static int M, N, sum;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        ArrayList<Integer> result = new ArrayList<>();
 
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        path = new int[M][N];
         visited = new boolean[M][N];
 
-        for (int i = 0; i < K; i++) {
+        for(int i=0; i<K; i++) {
             st = new StringTokenizer(br.readLine());
+
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
+
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            for (int j = y1; j < y2; j++) {
-                for (int k = x1; k < x2; k++) {
-                    path[j][k] = 1;
+            for(int x = M-y2; x<=M-y1-1; x++) {
+                for(int y = x1; y<=x2-1; y++) {
+                    if(visited[x][y]) continue;
+
+                    visited[x][y] = true;
                 }
             }
         }
 
-        ArrayList<Integer> areas = new ArrayList<>();
         int count = 0;
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j] && path[i][j] == 0) {
-                    areas.add(bfs(i, j));
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
+                if(!visited[i][j]) {
                     count++;
+                    sum = 0;
+                    dfs(i,j);
+                    result.add(sum);
                 }
             }
         }
 
-        Collections.sort(areas);
-        System.out.println(count);
-        for (int area : areas) {
-            System.out.print(area + " ");
+        Collections.sort(result);
+
+        for(int num : result) {
+            sb.append(num).append(" ");
         }
+
+        System.out.println(count);
+        System.out.print(sb);
+
     }
 
-    static int bfs(int x, int y) {
-        int areaSize = 1;
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y));
+    static void dfs(int x, int y) {
+        sum++;
         visited[x][y] = true;
 
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            int curX = node.x;
-            int curY = node.y;
+        for(int i=0; i<4; i++) {
+            int tempX = x + dx[i];
+            int tempY = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
+            if(tempX<0 || tempX>=M || tempY<0 || tempY>=N) continue;
 
-                if (nx >= 0 && nx < M && ny >= 0 && ny < N && !visited[nx][ny] && path[nx][ny] == 0) {
-                    visited[nx][ny] = true;
-                    q.add(new Node(nx, ny));
-                    areaSize++;
-                }
-            }
+            if(visited[tempX][tempY]) continue;
+
+            dfs(tempX, tempY);
         }
-        return areaSize;
-    }
-}
-
-class Node {
-    int x;
-    int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
