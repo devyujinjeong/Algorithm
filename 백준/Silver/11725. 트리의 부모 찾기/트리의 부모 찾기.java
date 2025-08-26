2aa3302a352d0e897bedfeb1adaa26a38c24d957
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-08-26 15:48:05 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 524 ms, Memory: 60068 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,57 +1,63 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static ArrayList<Integer>[] A; // 인접 배열을 저장할 리스트
-	static int[] result; // 각각의 부모들을 저장할 리스트
-	static boolean[] visited; // 방문 여부
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N = Integer.parseInt(br.readLine());
-				
-		A = new ArrayList[N+1];
-		visited = new boolean[N+1];
-		result = new int[N+1];
-		
-		for (int i = 1; i < N+1; i++) {
-			A[i] = new ArrayList<Integer>();
-		}
-		
-		for (int i = 0; i < N-1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			A[s].add(e);
-			A[e].add(s);
-		}
-		
-		DFS(1);
-		
-		for(int i=2; i<N+1; i++) {
-			bw.write(result[i]+"\n");
-		}
-		
-		bw.flush();
-		bw.close();
-	}
-	
-	static void DFS(int v) {
-		if(visited[v]) {return;}
-		
-		visited[v] = true;
-		for(int i : A[v]) {
-			if(!visited[i]) {
-				result[i] = v; // i의 부모는 v이니깐!
-				DFS(i);
-			}
-		}
-	}
+
+    static ArrayList<Integer>[] tree;
+    static boolean[] visited;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        tree = new ArrayList[N+1];
+        visited = new boolean[N+1];
+        parent = new int[N+1];
+
+        for(int i=1; i<=N; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<N-1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            tree[a].add(b);
+            tree[b].add(a);
+        }
+
+        bfs(1);
+
+        for(int i=2; i<=N; i++) {
+           sb.append(parent[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void bfs(int s) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        visited[s] = true;
+
+        while(!q.isEmpty()) {
+            int temp = q.poll();
+            for(int next : tree[temp]) {
+                if(visited[next]) continue;
+
+                q.add(next);
+                visited[next] = true;
+                parent[next] = temp;
+            }
+        }
+    }
 }
