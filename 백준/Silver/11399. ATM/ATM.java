3a2529cb4c23d926375b73596dcd1c2c4ef170c0
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-09-03 22:33:29 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,26 +5,27 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] A = new int[N];
-		int min = 0;
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
-		for(int i=0; i<N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(A);
-		
-		for(int i=0; i<N; i++) {
-			min += A[i]*(N-i);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		System.out.println(min);
-	}
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(P);
+
+        int total = 0;
+        int sum = 0;
+        for(int i=0; i<N; i++) {
+            sum+= P[i];
+            total+=sum;
+        }
+
+        System.out.println(total);
+
+    }
 }
