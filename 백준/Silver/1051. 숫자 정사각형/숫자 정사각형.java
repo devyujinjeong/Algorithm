ccecb4669300f64dfe1e5bdfaca5f096173d10c9
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-09-06 22:49:22 +0900

    [Silver III] Title: 숫자 정사각형, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,35 +12,31 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int min = Math.min(N,M);
-        int max = 1;
-
         int[][] tr = new int[N][M];
 
-        /* 1. 직사각형 만들기  */
         for(int i=0; i<N; i++) {
-            String num = br.readLine();
+            String s = br.readLine();
             for(int j=0; j<M; j++) {
-                tr[i][j] = num.charAt(j) - '0';
+                tr[i][j] = s.charAt(j) -'0';
             }
         }
 
-        /* 2. 크기가 큰 정사각형 부터 계산하기 */
-        for(int i= min-1; i>=1; i--) {
-            for(int j=0; i+j<N; j++) {
-                for(int k=0; k+i<M; k++) {
-                    int idx1 = j+i;
-                    int idx2 = k+i;
-                    if(tr[j][k] == tr[j][idx2] && tr[j][idx2] == tr[idx1][k] && tr[idx1][k] == tr[idx1][idx2]) {
-                        max = i+1;
-                        System.out.println(max * max);
-                        return;
+        int min = Math.min(N,M);
+
+        for(int len = min; len>=2; len--) {
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+
+                        if(i+len-1>=N || j+len-1>=M) continue; // 범위를 초과하는 경우
+
+                        if(tr[i][j]==tr[i+len-1][j] && tr[i+len-1][j]==tr[i][j+len-1]&&tr[i][j+len-1]==tr[i+len-1][j+len-1]) {
+                            System.out.println(len*len);
+                            return;
+                        }
                     }
                 }
-            }
         }
 
-        System.out.println(max);
-
+        System.out.println(1);
     }
 }
