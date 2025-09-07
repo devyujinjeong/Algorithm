126c4ac4cc9e04b3a57a317e5219e39f0762c30f
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-09-07 20:09:31 +0900

    [Silver I] Title: 금민수의 개수, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
@@ -1,12 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int count = 0;
-    static int A, B;
+    static int A,B;
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,19 +15,16 @@ public class Main {
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
 
-        calculate(4);
-        calculate(7);
-
+        findNum(0);
         System.out.println(count);
     }
 
-    static void calculate(long a) {
-        if(a>B) return; // B보다 a가 크다면 끝내기 (범위를 벗어남)
-        if(A<=a) count++; // A가 a보다 작거나 같다면 count++;
+    static void findNum(long cur){
+        if(cur>=A && cur<=B) count++;
 
-        // 그 다음 값 계산하기
-        // 4와 7로만 이루어진 수 이기 때문에 경우의 수는 다음과 같음
-        calculate(10*a+4);
-        calculate(10*a+7);
+        if(cur>B) return;
+
+        findNum(cur*10+4); // 숫자 4를 뒤에 붙이기
+        findNum(cur*10+7); // 숫자 7을 뒤에 붙이기
     }
 }
