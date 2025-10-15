4db6b5e2d41542e481bbb248ff6c546e9b2e110b
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-10-16 01:22:43 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 18152 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -9,17 +9,13 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[1000001];
 
-        dp[1] = 0; dp[2] = 1; dp[3] = 1;
-        for(int i=4; i<=N; i++) {
+        dp[1] = 0;
+        for(int i=2; i<=N; i++) {
             dp[i] = dp[i-1] + 1;
 
-            if(i%2==0) {
-                dp[i] = Math.min(dp[i/2]+1, dp[i]);
-            }
+            if(i%2==0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
 
-            if(i%3==0) {
-                dp[i] = Math.min(dp[i/3]+1, dp[i]);
-            }
+            if(i%3==0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
         }
 
         System.out.println(dp[N]);
