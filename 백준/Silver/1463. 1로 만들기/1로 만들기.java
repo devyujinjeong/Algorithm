5cb08894a3e0079acff70ef8927ab213ce99a36c
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-10-16 01:21:38 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,18 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] D = new int[N+1];
-		
-		D[1] = 0;
-		for(int i=2; i<=N; i++) {
-			D[i] = D[i-1]+1;
-			if(i%2==0) {D[i]=Math.min(D[i],D[i/2]+1);}
-			if(i%3==0) {D[i]=Math.min(D[i],D[i/3]+1);}
-		}
-		
-		System.out.println(D[N]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+
+        dp[1] = 0; dp[2] = 1; dp[3] = 1;
+        for(int i=4; i<=N; i++) {
+            dp[i] = dp[i-1] + 1;
+
+            if(i%2==0) {
+                dp[i] = Math.min(dp[i/2]+1, dp[i]);
+            }
+
+            if(i%3==0) {
+                dp[i] = Math.min(dp[i/3]+1, dp[i]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
 }
