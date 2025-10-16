d06825c3f328476178fedea488e646b1fe0014ff
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-10-16 20:23:04 +0900

    [level 2] Title: 숫자 변환하기, Time: 67.01 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -5,7 +5,7 @@ class Solution {
     static int[] dist;
     
     public int solution(int x, int y, int n) {
-        if(x==y) return 0;
+        if(x==y) return 0; // 숫자가 같은 경우에는 0을 return 
     
         dist = new int[y+1];
         Arrays.fill(dist, -1);
@@ -13,21 +13,21 @@ class Solution {
         return countMin(n, x, y);
     }
     
-    static int countMin(int n, int s, int e) {
+    static int countMin(int n, int x, int y) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(s);
-        dist[s] = 0;
+        q.add(x);
+        dist[x] = 0;
         
         while(!q.isEmpty()) {
             int cur = q.poll();
-            int nextDist = dist[cur] + 1;
+            int nextDist = dist[cur] + 1; // 다음 거리
             
             int[] nexts = {cur + n, cur * 2, cur * 3};
             for(int nx : nexts) {
-                if(nx > e) continue;
-                if(dist[nx] != -1) continue;
-                dist[nx] = nextDist;
-                if(nx == e) return nextDist;
+                if(nx > y) continue; // y를 초과하는 경우에는 확인할 필요 없음 
+                if(dist[nx] != -1) continue; // -1이 아니면 방문 했으므로 건너뛰기
+                dist[nx] = nextDist; // nextDist 저장하기
+                if(nx == y) return nextDist;
                 q.add(nx);
             }
         }
