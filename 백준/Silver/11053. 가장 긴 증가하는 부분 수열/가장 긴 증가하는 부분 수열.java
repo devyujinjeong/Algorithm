892d87df678089d0026aba3944a9abff67f3c985
yujin jeong <136146110+devyujinjeong@users.noreply.github.com>
2025-10-20 18:08:12 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,38 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] A = new int[N];
-		int[] dp = new int[N];
+        int N = Integer.parseInt(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
+        int[] num = new int[N];
+        int[] dp = new int[N];
+        
 
-		for (int i = 0; i < N; i++) {
-			dp[i] = 1;
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+        
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<i; j++) {
+                if(num[i] > num[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
 
-		for (int i = 1; i < N; i++) {
-			for (int j = 0; j < i; j++) {
-				if (A[j] < A[i]) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
-				}
-			}
-		}
+        int max = 0;
 
-		int max = 0;
-		for(int i=0; i<N; i++) {
-			max = Math.max(max, dp[i]);
-		}
-		
-		System.out.println(max);
-	}
+        for(int i=0; i<N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
+    }
 }
